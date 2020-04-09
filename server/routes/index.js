068352700f1c156d4a97158eeadf199cb22246cb
JordanTreDaniel <lilj390150@live.com>
2020-04-08 22:08:47 -0500

    Get accessToken from User, not session

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,15 @@ const router = express.Router();
 async function search(req, res, next) {
   const { query } = req;
   const { q } = query;
-  const { accessToken } = req.session;
+  const { headers } = req;
+  // const { accessToken } = req.session; //TO-DO: Get access token to be dependably stored in session, so we don't save on User.
+  const { authorization: accessToken } = headers;
   if (!accessToken) {
-    res.write(`<a href="/authorize">Sign in first</a>`);
+    res.status(401).json({ status: 401, statusText: "Missing access token. Please sign in first" });
+    // made a mistake with the line above and this helped me out:
+    //https://stackoverflow.com/questions/7042340/error-cant-set-headers-after-they-are-sent-to-the-client
   }
+
   axios({
     method: "get",
     url: `https://api.genius.com/search`,
