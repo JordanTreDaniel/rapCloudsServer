8d78cc03c0c4e1caa102b502ceb07aaa7ce51ebc
JordanTreDaniel <lilj390150@live.com>
2020-11-20 15:29:46 -0600

    Implement AWS Auth + Update login message

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express';
 var router = express.Router();
+import User from '../db/models/User';
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
+const updateUser = async (req, res, next) => {
+	try {
+		const { user } = req.body;
+		await User.findOneAndUpdate({ _id: user._id }, user, { new: true }).exec();
+		res.status(200).json({ user });
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+
+router.post('/updateUser', updateUser);
 
 export default router;
