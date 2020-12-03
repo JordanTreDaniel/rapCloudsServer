4c62f2bd96a2a6ff86926499bada8e73abb020d1
JordanTreDaniel <lilj390150@live.com>
2020-12-03 13:29:35 -0600

    update the mask schema

--- server/db/models/Mask.js
+++ server/db/models/Mask.js
@@ -3,7 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 const MaskSchema = new mongoose.Schema({
 	userId: String,
 	name: String,
-	info: Schema.Types.Mixed,
+	private: { type: Boolean, default: true },
+	info: Schema.Types.Mixed, //TO-DO: Make a schema for the info cloudinary gives
 });
 
 export default mongoose.model('Mask', MaskSchema);
