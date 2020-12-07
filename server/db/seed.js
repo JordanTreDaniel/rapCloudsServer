2af2ca2a8aca2cce13a86ba7214ed2b740d7bdfb
JordanTreDaniel <lilj390150@live.com>
2020-12-07 10:52:24 -0600

    Update seed function

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,27 +1,47 @@
 import Mask from './models/Mask';
 import fs from 'fs';
 const mongoose = require('mongoose');
+import cloudinary from 'cloudinary';
 
 const seedDB = async () => {
 	const collections = await mongoose.connection.collections;
 	const collectionNames = Object.keys(collections);
 	console.log('seeding', collectionNames);
-	if (collectionNames.includes('masks')) {
-		await Mask.collection.drop();
+	try {
+		const masks = await Mask.find({ userId: { $in: [ undefined, null ] } }).exec();
+		console.log(`There are ${masks.length} public masks to delete.`);
+		for (let maskIdx = 0; maskIdx < masks.length; maskIdx++) {
+			const mask = masks[maskIdx];
+			const { public_id } = mask.info;
+			console.log('deleting public_id', public_id);
+			await cloudinary.v2.uploader.destroy(public_id);
+			await mask.deleteOne();
+		}
+	} catch (error) {
+		console.log('Problem dropping mask collection', error);
 	}
-
 	const ignoreDir = fs.readdirSync('./ignore');
-	ignoreDir.forEach((fileName, idx) => {
+	for (let idx = 0; idx < ignoreDir.length; idx++) {
+		const fileName = ignoreDir[idx];
+		console.log(`Adding ${fileName} as a public mask.`);
 		const [ name, ext ] = fileName.split('.');
 		if (!name.length) {
-			return;
+			continue;
 		}
-		const mask = new Mask();
-		mask.name = name;
-		mask.img.data = fs.readFileSync(`./ignore/${fileName}`);
-		mask.img.contentType = `image/${ext}`;
-		mask.save();
-	});
+		const cloudinaryResult = await cloudinary.v2.uploader.upload(
+			`ignore/${fileName}`,
+			{ folder: '/publicMasks' },
+			(error, result) => {
+				if (error) {
+					console.log('Something went wrong while trying to save your Mask to Cloudinary.', error);
+					return error;
+				}
+				return result;
+			},
+		);
+		const mask = new Mask({ name, private: false, info: cloudinaryResult });
+		await mask.save();
+	}
 };
 
 export default seedDB;
