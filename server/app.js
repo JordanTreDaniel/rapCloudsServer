a1cbbc6a8e0cbc146761839acf9813c2f5a3601b
JordanTreDaniel <lilj390150@live.com>
2021-01-28 11:52:21 -0600

    Second CORS block

--- server/app.js
+++ server/app.js
@@ -32,12 +32,29 @@ const appRootUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:30
 // 		origin: appRootUrl, //TO-DO: Use env vars to distinguish b/t dev & prod
 // 	}),
 // );
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	console.log('Cors block', res);
-	next();
-});
+
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	console.log('Cors block', res);
+// 	next();
+// });
+
+const whitelist = [ 'https://www.rapclouds.com' ];
+app.use(
+	cors({
+		origin: function(origin, callback) {
+			console.log('Second cors block');
+			// allow requests with no origin
+			if (!origin) return callback(null, true);
+			if (whitelist.indexOf(origin) === -1) {
+				var message = `The CORS policy for this origin doesn't allow access from the particular origin.`;
+				return callback(new Error(message), false);
+			}
+			return callback(null, true);
+		},
+	}),
+);
 
 mongoose.connect(
 	`mongodb+srv://myself:${process.env.DB_PASSWORD}@cluster0-xlyk2.mongodb.net/${process.env.NODE_ENV === 'development'
