76ca651b70c0c76d3c532701fc7f000a4c992700
JordanTreDaniel <lilj390150@live.com>
2022-04-16 21:51:59 -0500

    Update RapCloudSettingsSchema

--- server/db/models/RapCloud.js
+++ server/db/models/RapCloud.js
@@ -1,42 +1,61 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const RapCloudSettingsSchema = new mongoose.Schema({
+	addWatermark: Boolean,
 	backgroundColor: String,
+	blackoutThreshold: Number,
 	collocations: Boolean,
 	coloredBackground: Boolean,
 	colorFromMask: Boolean,
-	colors: [ String ],
+	colors: [String],
 	contour: Boolean,
 	contourColor: String,
-	contourWidth: String,
+	contourWidth: Number,
+	currentFontName: String,
+	currentFontVariantIdx: Number,
 	detectEdges: Boolean,
-	downSample: String,
-	height: String,
+	downsample: Number,
+	fadeCloud: Boolean,
+	font: {
+		name: String,
+		addy: String,
+	},
+	fontDesired: Boolean,
+	height: Number,
 	includeNumbers: Boolean,
+	margin: Number,
 	maskAsBackground: Boolean,
 	maskDesired: Boolean,
 	maskId: Schema.Types.ObjectId,
+	cloudOpacity: Number,
+	maxFontSize: Number,
+	minFontSize: Number,
+	preferHorizontal: Number,
+	private: Boolean,
+	relativeScaling: Number,
 	repeat: Boolean,
-	stopWords: [ String ],
+	stopWords: [String],
 	transparentBackground: Boolean,
 	useCustomColors: Boolean,
 	useRandomColors: Boolean,
-	whiteThreshold: String,
-	width: String,
-	private: Boolean,
+	whiteThreshold: Number,
+	width: Number,
 });
 
 const RapCloudSchema = new mongoose.Schema({
-	artistIds: [ String ],
+	artistIds: [String],
 	description: String,
 	settings: RapCloudSettingsSchema,
-	songIds: [ String ], //NOTE: Not using Schema.Types.ObjectId here bc the id's are genius ids
+	songIds: [String], //NOTE: Not using Schema.Types.ObjectId here bc the id's are genius ids
 	userId: Schema.Types.ObjectId,
 	info: Schema.Types.Mixed, //TO-DO: Make a schema for the info cloudinary gives
 	inspirationType: String,
-	lyricString: { type: String, default: "Lyric string wasn't provided upon creation." },
+	lyricString: {
+		type: String,
+		default: "Lyric string wasn't provided upon creation.",
+	},
 	officialCloud: { type: Boolean, default: false },
 	private: { type: Boolean, default: true },
 });
 
-export default mongoose.model('RapCloud', RapCloudSchema);
+export default mongoose.model("RapCloud", RapCloudSchema);
